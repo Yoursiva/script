3f1d81847b15d98c71a479ecc4826e76aebced72
Yoursiva <136670949+Yoursiva@users.noreply.github.com>
2023-06-16 12:55:25 +0530

    Add files via upload

---
+++ dbo.Email.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Email]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	mailto varchar(250) NOT NULL,
+	cc varchar(250) NULL,
+	bcc varchar(250) NULL,
+	subject varchar(250) NOT NULL,
+	body varchar(250) NULL,
+	status varchar(250),
+)
---
+++ dbo.Template.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Template] (
+    [Id]       INT           NOT NULL IDENTITY(1,1),
+    [mailto]   VARCHAR (250) NOT NULL,
+    [cc]       VARCHAR (250) NULL,
+    [bcc]      VARCHAR (250) NULL,
+    [subject]  VARCHAR (250) NOT NULL,
+    [body]     VARCHAR (250) NULL,
+    [isactive] INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
